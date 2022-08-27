f0ae88bf31520f1318c127f1506ab4121b489b5d
Jorgelina Yapur <109431369+joryapur@users.noreply.github.com>
2022-08-27 00:38:29 -0300

    Update calculadora.js

--- Calculadora/js/calculadora.js
+++ Calculadora/js/calculadora.js
@@ -1,39 +1,14 @@
-
-/*Eventlistener que escucha cuando se carga el documento 
-html. Esto es para evitar que cuando preguntemos por display
-o botones nos diga que no existe porque si Js corre antes de que se
-cargue hmtl y busca un elemento que todavia no se creo va a decir
-que no exite.*/
-
 window.addEventListener('load', ()=>{
 
-    /*Dentro creamos las dos constantes para buscar y guardar
-     el display y los botones(dentro de un html collection)*/
+const display = document.querySelector('.calculator-display');
+const keypadButtons = document.getElementsByClassName('keypad-button');
+const keypadButtonsArray = Array.from(keypadButtons);
 
-   const display=document.querySelector('.calculator-display');
-   const keypadButtons= document.getElementsByClassName('keypad-button');
-   
-   /*Creamos otra constanta para convertir a Array el html collection 
-    de los botones que creamos antes y asi poder iterarlo*/
-
-   const keypadButtonsArray= Array.from(keypadButtons);
-
-   /*iteramos por el nuevo array de botones que se creo*/
-
-   keypadButtonsArray.forEach( (button) => {
-
-    /*A cada boton le agregamos un Listener para 
-    cuando se hace click*/
-
+keypadButtonsArray.forEach( (button) => {
       button.addEventListener( 'click', ()=> {
-         /*imprime el html que tiene adentro cada boton*/
-        /*console.log(button.innerHTML);*/
         calculadora(button,display)
-      
-      })
-
-   })
-   
+       })
+    })   
 })
 
 function calculadora(button, display){
@@ -53,15 +28,14 @@ function calculadora(button, display){
 }
 
 function calcular(display){
-    display.innerHTML= eval(display.innerHTML) /*toma el string de lo que hay en el display y lo resuelve y lo guarda en el innerhtml del display)*/
-     
+    display.innerHTML= eval(display.innerHTML) 
 }
 
 function actualizar(display,button){
     if (display.innerHTML == 0) {
-        display.innerHTML = ""; /*las comillas es para dejar un string vacio, sacar el 0*/
+        display.innerHTML = "";
     }
-       /*para mostrar en el display los botones que vayamos tocando uno al lado del otro*/
+     
     display.innerHTML += button.innerHTML
 }
 
